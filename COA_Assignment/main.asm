1564dcb18cafd5bf6bf27774669746c40cc950b0
YKL-27 <ykliang88@gmail.com>
2024-09-06 23:10:30 +0800

    YK: Setup basic frame

--- COA_Assignment/main.asm
+++ COA_Assignment/main.asm
@@ -5,10 +5,18 @@ Include Irvine32.inc
 ExitProcess proto,dwExitCode:dword
 
 .data
+;------------------------------------------DISPLAY MESSAGES
+
+
+;------------------------------------------VARIABLES
 
 .code
 main proc
-
+    login:
+    page1:
+    page2:
+    page3:
+    
     invoke ExitProcess,0
 main endp
 end main
