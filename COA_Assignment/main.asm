16b69ca9f13df2ea76266b00ecb78afed9bfe0c8
YKL-27 <ykliang88@gmail.com>
2024-09-08 22:44:46 +0800

    QX: Added admin login

--- COA_Assignment/main.asm
+++ COA_Assignment/main.asm
@@ -1,22 +1,98 @@
-Include Irvine32.inc
-.386
-.model flat,stdcall
-.stack 4096
-ExitProcess proto,dwExitCode:dword
+INCLUDE Irvine32.inc
 
 .data
 ;------------------------------------------DISPLAY MESSAGES
-
-
+usernamePrompt  BYTE "Enter username: ", 0
+passwordPrompt  BYTE "Enter password: ", 0
+successMsg      BYTE "Login successful!", 0
+failMsg         BYTE "Invalid username or password!", 0
 ;------------------------------------------VARIABLES
+username        BYTE 20 DUP(0)   
+password        BYTE 20 DUP(0)   
+correctUsername BYTE "user123", 0  
+correctPassword BYTE "pass123", 0 
 
 .code
-main proc
+main PROC
     login:
-    page1:
-    page2:
-    page3:
+        ; Display the username prompt
+        mov edx, OFFSET usernamePrompt
+        call WriteString
     
+        ; Read username input
+        mov edx, OFFSET username
+        mov ecx, 20  ; Max length of input
+        call ReadString
+    
+        ; Display the password prompt
+        mov edx, OFFSET passwordPrompt
+        call WriteString
+    
+        ; Read the password input
+        mov edx, OFFSET password
+        mov ecx, 20  
+        call ReadString
+
+        ; Compare the entered username with the correct username
+        mov esi, OFFSET username
+        mov edi, OFFSET correctUsername
+        call StrCompareCustom
+        cmp eax, 0
+        jne loginFailed 
+    
+        ; Compare the entered password with the correct one
+        mov esi, OFFSET password
+        mov edi, OFFSET correctPassword
+        call StrCompareCustom
+        cmp eax, 0
+        jne loginFailed 
+
+        ; If both match, login successful
+        mov edx, OFFSET successMsg
+        call WriteString
+        jmp endProgram
+
+    loginFailed:
+        ; Display failure message
+        mov edx, OFFSET failMsg
+        call WriteString
+
+    endProgram:
+        ; Exit program
+        call WaitMsg
+        exit
+
+    ; Custom string comparison function
+    StrCompareCustom PROC
+        ; Compares two strings pointed by ESI and EDI
+        ; Returns 0 if strings are equal, 1 otherwise
+    
+        push esi    ; 
+        push edi
+    compareLoop:
+        mov al, [esi]    
+        mov bl, [edi]    
+        cmp al, bl     
+        jne notEqual     
+        test al, al       
+        je equal         
+        inc esi         
+        inc edi
+        jmp compareLoop  
+
+    notEqual:
+        mov eax, 1       
+        jmp done
+
+    equal:
+        mov eax, 0       
+
+    done:
+        pop edi          
+        pop esi
+        ret
+
+    StrCompareCustom ENDP
     invoke ExitProcess,0
-main endp
-end main
+main ENDP
+END main
